5a729931db2e26f3a79ae45bf8c4e47653621096
Ponfee <ponfee.cn@gmail.com>
2020-05-22 12:51:27 +0800

    optimization

---
+++ src/main/java/code/ponfee/commons/extract/ExtractableDataSource.java
@@ -0,0 +1,38 @@
+package code.ponfee.commons.extract;
+
+import java.io.Closeable;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.annotation.Nonnull;
+
+/**
+ * The extractable DataSource is an Inputstream or File
+ * 
+ * @author Ponfee
+ */
+public class ExtractableDataSource implements Closeable {
+    private final Object dataSource;
+
+    public ExtractableDataSource(@Nonnull Object dataSource) {
+        if (!(dataSource instanceof InputStream || dataSource instanceof File)) {
+            throw new IllegalArgumentException(
+                "Invalid datasource '" + dataSource.getClass().getName() + "', it must be File or InputStream."
+            );
+        }
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (dataSource instanceof InputStream) {
+            ((InputStream) dataSource).close();
+        }
+    }
+
+    public Object getDataSource() {
+        return dataSource;
+    }
+
+}
